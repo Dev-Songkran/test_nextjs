80f4cfc316fa6e23c71569de39d0e76bfe414331
s.wichitpong <s.wichitpong@gmail.com>
2019-09-03 13:07:55 +0700

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,49 @@
-import Link, { prefetch } from '../components/link'
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import loadDB from '../lib/load-db'
 
-// we just render a list of 3 articles having 2 with prefetched data
-export default () => (
-  <main>
-    <h1>Next.js - with data prefetch example</h1>
-    <ul>
-      <li>
-        <Link href='/article?id=1' prefetch withData>
-          <a>Article 1</a>
-        </Link>
-      </li>
-      <li>
-        <Link href='/article?id=2' prefetch>
-          <a>Article 2</a>
-        </Link>
-      </li>
-      <li>
-        <Link href='/article?id=3'>
-          <a onMouseOver={e => prefetch('/article?id=3')}>Article 3</a>
-        </Link>
-      </li>
-    </ul>
-  </main>
-)
+function PostLink(props) {
+  return (
+    <li>
+      <Link as={`/p/${props.id}`} href={`/post?id=${props.id}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
+}
+
+function Index({ stories }) {
+  return (
+    <Layout>
+      <h1>Hacker News - Latest</h1>
+      <ul>
+        {stories.map(story => (
+          <PostLink key={story.id} id={story.id} title={story.title} />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
+
+Index.getInitialProps = async function() {
+  const db = await loadDB()
+
+  const ids = await db.child('topstories').once('value')
+  let stories = await Promise.all(
+    ids
+      .val()
+      .slice(0, 10)
+      .map(id =>
+        db
+          .child('item')
+          .child(id)
+          .once('value')
+      )
+  )
+
+  stories = stories.map(s => s.val())
+
+  return { stories }
+}
+
+export default Index
